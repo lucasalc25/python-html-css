17ed27fdf5b9d8f77903b1426b8fdf153cb64490
lucasalc25 <lucas.alc25@gmail.com>
2022-06-01 00:54:23 +0000

    aula03

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, flash, session, redirect, url_for, abort
 import sqlite3
 
 DATABASE = "banco.bd"
@@ -10,22 +10,48 @@ app.config.from_object(__name__)
 def conecta_bd():
     return sqlite3.connect(DATABASE)
 
+@app.before_request
+def antes_requisicao():
+    g.bd = conecta_bd()
 
-@app.before_request # Sempre antes do app iniciar uma requisição 
-def antes_requisicao(): # Cria e chama a função 
-    g.bd = conecta_bd() # Guarda a conexão com o banco na variável g 
+@app.teardown_request
+def depois_requisicao(e):
+    g.bd.close()
 
-
-@app.teardown_request # Sempre depois do app iniciar a requisição 
-def depois_requisicao(e): # Cria e chama a função 
-    g.bd.close() # Fecha a conexão com o banco
-
-@app.route("/") # Cria a rota do app
-def exibir_entradas(): # Cria e chama a função
-    sql = "SELECT titulo, texto, criado_em FROM entradas"
+@app.route("/")
+def exibir_entradas():
+    sql = "SELECT titulo, texto, criado_em FROM entradas ORDER BY id DESC;"
     cur = g.bd.execute(sql)
     entradas = []
     for titulo, texto, criado_em in cur.fetchall():
         entradas.append({"titulo": titulo, "texto": texto, "criado_em": criado_em})
-    return  render_template("layout.html", entradas = entradas ) # Retorna o arquivo html 'hello'
+    return render_template("exibir_entradas.html", entradas=entradas)
 
+@app.route('/inserir', methods=["POST"])
+def inserir_entradas():
+    if not session.get('logado'):
+        abort(401)
+    titulo = request.form['titulo']
+    texto = request.form['texto']
+    sql = "INSERT INTO entradas(titulo, texto) VALUES (?, ?)"
+    g.bd.execute(sql, [titulo, texto]) 
+    g.bd.commit()  
+    flash("Post gravado com Sucesso!")
+    return redirect(url_for("exibir_entradas"))
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form['username'] == "admin" and request.form['password'] == "admin":
+            session['logado'] = True
+            flash("Login efetuado!")
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou Senha inválidos"
+    return render_template("login.html", erro=erro)
+
+@app.route("/logout")
+def logout():
+    session.pop('logado', None)
+    flash("Logout Efetuado com Sucesso!")
+    return redirect(url_for("exibir_entradas"))
